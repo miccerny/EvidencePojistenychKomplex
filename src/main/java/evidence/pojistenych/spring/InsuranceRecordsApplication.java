3c1e9907bd48f91b91f88a9b41c01cff78ff3b3f
Michal Černý <cerny.michal93@gmail.com>
2025-02-16 00:06:37 +0100

    Prvni

---
+++ src/main/java/evidence/pojistenych/spring/InsuranceRecordsApplication.java
@@ -0,0 +1,12 @@
+package evidence.pojistenych.spring;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceRecordsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceRecordsApplication.class, args);
+    }
+}
