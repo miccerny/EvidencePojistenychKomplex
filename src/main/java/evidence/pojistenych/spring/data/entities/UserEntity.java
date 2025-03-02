f58502698f8b71e1d9161884a3847ce078266e83
Michal Černý <cerny.michal93@gmail.com>
2025-03-02 12:27:19 +0100

    Merge remote-tracking branch 'origin/master'

--- src/main/java/evidence/pojistenych/spring/data/entities/UserEntity.java
+++ src/main/java/evidence/pojistenych/spring/data/entities/UserEntity.java
@@ -1,55 +1,29 @@
 package evidence.pojistenych.spring.data.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 @Entity
 public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long  insuredId;
-
-    @Column(nullable = false)
-    @Size(min = 3, message = "Vyplňte platné jméno")
-    private String firstName;
-
-    @Column(nullable = false)
-    @Size(min = 3, message = "Vyplňte platné příjmení")
-    private String lastName;
-
+    private long userId;
 
     @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
-    private String phoneNumber;
+    private String password;
 
     @Column(nullable = false)
-    private String street;
+    private boolean admin;
 
-    @Column(nullable = false)
-    private String city;
-
-    @Column(nullable = false)
-    @Size(min = 6, max = 6, message = "Vyplňte validní PSČ")
-    private int zipcode;
-
-    public String getFirstName() {
-        return firstName;
+    public long getUserId() {
+        return userId;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setUserId(long userId) {
+        this.userId = userId;
     }
 
     public String getEmail() {
@@ -60,35 +34,19 @@ public class UserEntity {
         this.email = email;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
+    public String getPassword() {
+        return password;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public String getStreet() {
-        return street;
+    public boolean isAdmin() {
+        return admin;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public int getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(int zipcode) {
-        this.zipcode = zipcode;
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
     }
 }
