eca051470023dcded37ee08d62547fe276e36c40
Michal Černý <cerny.michal93@gmail.com>
2025-03-15 13:19:08 +0100

    upravy

--- src/main/java/evidence/pojistenych/spring/InsuranceRecordsApplication.java
+++ src/main/java/evidence/pojistenych/spring/InsuranceRecordsApplication.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 @SpringBootApplication
 @EnableJpaRepositories
-public class InsuranceRecordsApplication {
+public class  InsuranceRecordsApplication {
     public static void main(String[] args) {
         SpringApplication.run(InsuranceRecordsApplication.class, args);
     }
